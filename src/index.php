25e90af5d8258a04e2fcb81584ad9653839a4f03
Diogo Souza <diogo@diogok.net>
2014-10-20 14:39:48 -0200

    api básica e index.html placeholder

--- src/index.php
+++ src/index.php
@@ -6,5 +6,19 @@ include '../config.php';
 
 use Respect\Rest\Router;
 
-$r = new Router
+$r = new Router;
+
+$r->get('/api/steps',function(){
+	$repo = new \enade\repository\StepRepo;
+	$steps = $repo->getAll();
+	$json = json_encode($steps);
+	header('Content-Type: application/json');
+	return $json;
+});
+
+
+$r->get('/',function(){
+    header('Location: index.html');
+});
+
 
