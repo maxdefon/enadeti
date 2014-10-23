54b1cc935a476e9c0f36969e39ac98657d433a4f
Diogo Silva <diogo@diogok.net>
2014-10-22 23:28:16 -0200

    cuidando do CORS no PHP

--- src/index.php
+++ src/index.php
@@ -1,6 +1,20 @@
 <?php
 session_start();
 
+if(isset($_SERVER['HTTP_ORIGIN'])) {
+    header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
+    header('Access-Control-Allow-Headers: X-Requested-With');
+    header('Access-Control-Allow-Headers: Content-Type');
+}
+
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+    if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
+        header("Access-Control-Allow-Methods: GET, POST, OPTIONS");         
+    if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
+        header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
+    exit(0);
+}
+
 require '../vendor/autoload.php';
 include '../config.php';
 
